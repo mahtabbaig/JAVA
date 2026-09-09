f912ca10366bb615f434016f4ed9ca6b710a33a5
mahtabbaig190@gmail.com <mahtabbaig190@gmail.com>
2026-09-09 10:26:00 +0530

    next problems

---
+++ natu.java
@@ -0,0 +1,10 @@
+public class natu {
+    public static void main(String[] args) {
+        int n=5;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
